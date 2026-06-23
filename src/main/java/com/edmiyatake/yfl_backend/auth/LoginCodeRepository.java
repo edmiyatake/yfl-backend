5f88ac49bc3464287f49b4c88507d71c95b8ed3c
Edwin Miyatake <edmiyatake@gmail.com>
2026-06-22 19:39:25 -0700

    Create LoginCodeRepository.java

---
+++ src/main/java/com/edmiyatake/yfl_backend/auth/LoginCodeRepository.java
@@ -0,0 +1,8 @@
+package com.yfl_backend.auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface LoginCodeRepository extends JpaRepository<LoginCode, Long> {
+    Optional<LoginCode> findByEmail(String email);
+}
