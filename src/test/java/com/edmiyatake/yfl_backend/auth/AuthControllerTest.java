2087e2221cc5fce75dce0fced3efa308f85d11ee
Edwin Miyatake <edmiyatake@gmail.com>
2026-06-22 19:48:48 -0700

    Update AuthControllerTest.java
    remove spring 3 import

--- src/test/java/com/edmiyatake/yfl_backend/auth/AuthControllerTest.java
+++ src/test/java/com/edmiyatake/yfl_backend/auth/AuthControllerTest.java
@@ -3,16 +3,18 @@ package com.edmiyatake.yfl_backend.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 
 import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@AutoConfigureTestRestTemplate
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthControllerTest {
 
